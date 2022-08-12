dee05cbf9ea05a125b89fa96ce4ff8c13ece5625
alber <alber@ALBERTO_BARBOSA>
2022-08-12 12:27:40 -0300

    Adicionar arquivos de projeto.

---
+++ Diabetes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Glicose;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        double glicose;
+
+        Console.Write("Digite por favor a medida da glicose: ");
+        glicose = double.Parse(Console.ReadLine());
+
+        if(glicose <= 100)
+        {
+            Console.WriteLine("Classificação: Normal");
+        }
+        else if(glicose <= 140)
+        {
+            Console.WriteLine("Classificação: Elevado");
+        }
+        else if(glicose >= 140)
+        {
+            Console.WriteLine("Classificação: Diabetes");
+        }
+    }
+}
